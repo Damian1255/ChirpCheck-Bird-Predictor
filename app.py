46a5d9a6d36c3745f8e80366ad7aea696ab98ffb
Damian12555 <daminlau125@gmail.com>
2024-02-09 01:47:28 +0800

    wip

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, session, request, jsonify
-import src.predictor as p
+import src.predictor as predictor
+import src.processing as processing
 
-predictor = p.Predictor()
+predictor = predictor.Predictor()
+processor = processing.Processing()
 
 # Initialize Flask app
 app = Flask(__name__, static_url_path='/static')
@@ -13,11 +15,12 @@ def index():
 @app.route('/', methods=['POST'])
 def predict():
     text = request.form['text']
-    processed_text = text.lower()   
+    # processed_text = text.lower()
+    processed_text = processor.preprocess_text(text)
     prediction, confidence = predictor.predict(processed_text)
 
     return render_template('index.html', result=prediction, confidence=confidence)
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
---
+++ src/processing.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import contractions
+import string
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+class Processing:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
+        self.punctuation = string.punctuation
+        self.contractions = contractions
+
+    def preprocess_text(self, text):
+        # Lowercase the text
+        text = text.lower()
+        # Expand contractions
+        text = ' '.join([self.contractions.fix(word) for word in text.split()])
+        # Remove punctuation
+        text = ''.join([char for char in text if char not in self.punctuation])
+        # Tokenize the text
+        tokens = word_tokenize(text)
+        # Remove stop words
+        tokens = [word for word in tokens if word not in self.stop_words]
+        # remove token that is 2 characters or less
+        tokens = [word for word in tokens if len(word) > 2]
+        # POS tagging
+        pos_tags = nltk.pos_tag(tokens)
+        # Lemmatize the text using the POS tags
+        text = ' '.join([self.lemmatizer.lemmatize(word, self.get_wordnet_pos(pos_tag)) for word, pos_tag in pos_tags])
+        
+        return text
+    
+    def get_wordnet_pos(self, tag):
+        if tag.startswith('J'):
+            return nltk.corpus.wordnet.ADJ
+        elif tag.startswith('V'):
+            return nltk.corpus.wordnet.VERB
+        elif tag.startswith('N'):
+            return nltk.corpus.wordnet.NOUN
+        elif tag.startswith('R'):
+            return nltk.corpus.wordnet.ADV
+        else:
+            return nltk.corpus.wordnet.NOUN
